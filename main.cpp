577343f73bbddfed110324d9b320a10cb6d7952b
KenethSandoval <kenethra.74@gmail.com>
2021-09-15 17:57:06 -0600

    feat: create file .ppm

--- main.cpp
+++ main.cpp
@@ -1,6 +1,36 @@
 #include <iostream>
+#include <filesystem>
+#include <fstream>
+#include <complex>
+#include <cmath>
 
-int main() {
-  std::cout << "Hello";
-  return 1;
+constexpr auto WIDTH        = 1080;
+constexpr auto HEIGHT       = 650;
+constexpr auto FILE_SIZE    = WIDTH * HEIGHT;
+constexpr auto RAD          = 50;
+constexpr auto SCALE_FACTOR = 0.004;
+
+void writeToFile(std::array<int, FILE_SIZE> &data)
+{
+  const std::filesystem::path path = "./out.ppm";
+  std::ofstream out {path};
+  out << "P6\n" << WIDTH << " " << HEIGHT << " " << "255\n";
+
+  for(int i = 0; i < FILE_SIZE; ++i) {
+    const char pixel[3] = {
+      static_cast<const char>(data[i] >> (8 * 2) & 0xFF),
+      static_cast<const char>(data[i] >> (8 * 1) & 0xFF),
+      static_cast<const char>(data[i] >> (8 * 0) & 0xFF)
+    };
+    out.write(pixel, 3);
+  }
+  out.close();
+}
+
+
+int main() 
+{
+  std::array<int, FILE_SIZE> pixels{};
+  writeToFile(pixels);
+  return 0;
 }
